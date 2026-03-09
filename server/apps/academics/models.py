4bff557b37feee80b5168da109f70dc44148316f
CCVela1017 <carlosvelaeduardo@gmail.com>
2026-03-08 20:02:45 -0600

    feat: created teacher and semester models

--- server/apps/academics/models.py
+++ server/apps/academics/models.py
@@ -13,3 +13,24 @@ class Course(models.Model):
     name = models.CharField(max_length=200)
     cost_center = models.ForeignKey(CostCenter, on_delete=models.CASCADE)
     
+class Teacher(models.Model):
+    identity_code = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=200)
+    created_at = models.DateField()
+    is_active = models.BooleanField(default=True)
+
+class Semester(models.Model):
+
+    STATUS_CHOICES = [
+        ("uploading", "Uploading"),
+        ("processed", "Processed"),
+        ("archived", "Archived"),
+    ]
+
+    year = models.IntegerField()
+    number = models.IntegerField()
+    ceat_loaded = models.BooleanField(default=False)
+    comments_loaded = models.BooleanField(default=False)
+    control_loaded = models.BooleanField(default=False)
+    evaluation_loaded = models.BooleanField(default=False)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="uploading")
