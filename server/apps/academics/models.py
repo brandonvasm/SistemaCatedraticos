b0f1cafbbe464c62aa7f59d661c0dc42c7a16172
Carlosd365 <carlitosgoag3@gmail.com>
2026-03-15 02:25:25 -0600

    feat(apps/academics): add pensum loaded boolean to faculty

--- server/apps/academics/models.py
+++ server/apps/academics/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Faculty(models.Model):
     name = models.CharField(max_length=200)
+    pensum_loaded = models.BooleanField(default=False)
 
 class CostCenter(models.Model):
     name = models.CharField(max_length=200)
