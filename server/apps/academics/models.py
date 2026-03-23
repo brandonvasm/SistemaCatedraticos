c0a6ccefe65e78496894ad84a18b46abc5b3e237
CCVela1017 <carlosvelaeduardo@gmail.com>
2026-03-22 19:55:35 -0600

    feat: created course section model

--- server/apps/academics/models.py
+++ server/apps/academics/models.py
@@ -16,7 +16,6 @@ class Course(models.Model):
     name = models.CharField(max_length=200)
     cost_center = models.ForeignKey(CostCenter, on_delete=models.CASCADE)
 
-
 class Teacher(models.Model):
     identity_code = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=200)
@@ -44,8 +43,24 @@ class Semester(models.Model):
         Faculty, on_delete=models.SET_NULL, null=True, blank=True
     )
 
+class CourseSection(models.Model):
+
+    SHIFT_CHOICES = [
+        ("matutina", "Matutina"),
+        ("vespertina", "Vespertina"),
+        ("fin de semana", "Fin de Semana"),
+    ]
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    section_number = models.CharField(max_length=20)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True, blank=True)
+    assigned_students = models.IntegerField(default=0)
+    shift = models.CharField(max_length=20, choices=SHIFT_CHOICES)
+
 class Contract(models.Model):
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
     created_at = models.DateField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
+    
