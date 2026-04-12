62636821cc5b31c0e85dcfb056d973e254cfc454
CCVela1017 <carlosvelaeduardo@gmail.com>
2026-04-11 20:11:24 -0600

    feat: drf method to process a selected file

--- server/apps/files/views.py
+++ server/apps/files/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.decorators import action
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, status
 from rest_framework.response import Response
@@ -12,7 +13,7 @@ from django.db import transaction
 from .application.upload_file_use_case import UploadFileUseCase
 from .application.save_file_use_case import SaveFileUseCase
 from .application.validate_request_file import ValidateFileRequestUseCase
-
+from .application.process_excel import ProcessExcelUseCase
 
 
 # Create your views here.
@@ -56,6 +57,7 @@ class FileView(viewsets.ModelViewSet):
             return Response({"error": "Error saving file record: " + str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         # subir archivo a la nube
+
         upload_result = None
         try:
             upload_result = uploader.execute()
@@ -69,3 +71,18 @@ class FileView(viewsets.ModelViewSet):
         registered_file.save()
         return Response(file_record, status=status.HTTP_201_CREATED)
         
+    @action(detail=True, methods=['post'], url_path='process')
+    def process_file(self, request, pk=None):
+        file_record = self.get_object()
+        file_path = file_record.file.url
+        file_type = file_record.format
+
+        use_case = ProcessExcelUseCase()
+        try:
+            basic_info, records = use_case.execute(file_path=file_path, file_type=file_type)
+            return Response({
+                "basic_info": basic_info,
+                "records": records
+            }, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error": "Error processing file: " + str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
