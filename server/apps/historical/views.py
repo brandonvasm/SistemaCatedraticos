690b155f412e3e600cc371a33b71411e1173e013
CCVela1017 <carlosvelaeduardo@gmail.com>
2026-04-20 20:41:47 -0600

    feat: updated course history endpoint

--- server/apps/historical/views.py
+++ server/apps/historical/views.py
@@ -1,16 +1,22 @@
-# views.py
-from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.decorators import action
-from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiExample
-from django.db.models import Subquery, OuterRef
-from itertools import groupby
+from collections import defaultdict
 
-from .models import CourseHistory
+from drf_spectacular.utils import OpenApiExample, OpenApiResponse, extend_schema
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from apps.evaluations.models import SectionControl
+from apps.users.infrastructure.authentication import CookieJWTAuthentication
+from apps.users.infrastructure.permissions import IsSysAdminOrCoordinator
+
 from .domain.courses_history_serializers import CourseEvolutionSerializer
+from .models import CourseHistory
 
 
 class CourseHistoryViewSet(viewsets.ViewSet):
+    authentication_classes = [CookieJWTAuthentication]
+    permission_classes = [IsSysAdminOrCoordinator]
+
     @extend_schema(
         summary="Course evolution",
         responses={
@@ -39,39 +45,58 @@ class CourseHistoryViewSet(viewsets.ViewSet):
     )
     @action(detail=False, methods=["get"], url_path="evolution")
     def evolution(self, request):
-        last_3_semester_ids = (
-            CourseHistory.objects
+        user_faculty = request.user.faculty_id
+
+        last_3_semester_ids = list(
+            CourseHistory.objects.filter(course__cost_center__faculty=user_faculty)
             .values_list("semester_id", flat=True)
             .distinct()
-            .order_by("-semester__year", "-semester__number")
-            [:3]
+            .order_by("-semester__year", "-semester__number")[:3]
         )
 
-        histories = (
-            CourseHistory.objects
-            .filter(semester_id__in=last_3_semester_ids)
-            .select_related("course", "semester")
-            .order_by("course_id", "-semester__year", "-semester__number")
+        controls = SectionControl.objects.filter(
+            course_section__semester_id__in=last_3_semester_ids,
+            course_section__course__cost_center__faculty=user_faculty,
+        ).values(
+            "course_section__course_id",
+            "course_section__course__name",
+            "course_section__semester__year",
+            "course_section__semester__number",
+            "high_count",
+            "medium_count",
+            "low_count",
         )
 
-        data = []
-        for course_id, group in groupby(histories, key=lambda h: h.course_id):
-            entries = list(group)
-            data.append(
-                {
-                    "course_id": course_id,
-                    "course_name": entries[0].course.name,  
-                    "semester_ratings": [
-                        {
-                            "rating": entry.control_high_count,
-                            "semester_year": entry.semester.year,
-                            "semester_number": entry.semester.number,
-                        }
-                        for entry in entries
-                    ],
-                }
+        course_names = {}
+        section_scores = defaultdict(lambda: defaultdict(list))
+
+        for ctrl in controls:
+            course_id = ctrl["course_section__course_id"]
+            course_names[course_id] = ctrl["course_section__course__name"]
+            semester_key = (
+                ctrl["course_section__semester__year"],
+                ctrl["course_section__semester__number"],
             )
+            total = ctrl["high_count"] + ctrl["medium_count"] + ctrl["low_count"]
+            if total > 0:
+                section_scores[course_id][semester_key].append(
+                    (ctrl["high_count"] / total) * 100
+                )
 
-        serializer = CourseEvolutionSerializer(data, many=True)
-        return Response(serializer.data)
-    
+        data = [
+            {
+                "course_id": course_id,
+                "course_name": course_names[course_id],
+                "semester_ratings": [
+                    {
+                        "rating": round(sum(scores) / len(scores), 2),
+                        "semester_year": year,
+                        "semester_number": number,
+                    }
+                    for (year, number), scores in sorted(semester_data.items())
+                ],
+            }
+            for course_id, semester_data in section_scores.items()
+        ]
+
+        return Response(CourseEvolutionSerializer(data, many=True).data)
