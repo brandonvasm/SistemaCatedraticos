f0199571a9b1995c0e0742187face4c697404620
CCVela1017 <carlosvelaeduardo@gmail.com>
2026-05-01 20:03:34 -0600

    feat: analytic models for all contexts

--- server/apps/analytics/models.py
+++ server/apps/analytics/models.py
@@ -1,17 +1,30 @@
 from django.db import models
 
-# Create your models here.
-class TeacherAnalysisAI(models.Model):
-    PERCEPTION_CHOICES = [
-        ("positive", "Positive"),
-        ("neutral", "Neutral"),
-        ("negative", "Negative"),
-    ]
+class PerceptionChoices(models.TextChoices):
+    POSITIVE = "positive", "Positive"
+    NEUTRAL = "neutral", "Neutral"
+    NEGATIVE = "negative", "Negative"
 
+# Create your models here.
+class TeacherCommentAnalysisAI(models.Model):
     course_section = models.ForeignKey("academics.CourseSection", on_delete=models.CASCADE)
     ai_score = models.FloatField()
     comment_overview = models.TextField(blank=True, null=True)
-    comment = models.TextField(blank=True, null=True)
+    comment = models.TextField(blank=True, null=True, max_length=50)
     model_version = models.CharField(max_length=50)
-    perception = models.CharField(max_length=20, choices=PERCEPTION_CHOICES, default="neutral")
+    perception = models.CharField(max_length=20, choices=PerceptionChoices.choices, default="neutral")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class TeacherProfileAnalysisAI(models.Model):
+    teacher = models.ForeignKey("academics.Teacher", on_delete=models.CASCADE)
+    profile_overview = models.TextField(blank=True, null=True, max_length=50)
+    model_version = models.CharField(max_length=50)
+    perception = models.CharField(max_length=20, choices=PerceptionChoices.choices, default="neutral")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CourseAnalysisAI(models.Model):
+    course = models.ForeignKey("academics.Course", on_delete=models.CASCADE)
+    course_overview = models.TextField(blank=True, null=True, max_length=50)
+    model_version = models.CharField(max_length=50)
+    perception = models.CharField(max_length=20, choices=PerceptionChoices.choices, default="neutral")
     created_at = models.DateTimeField(auto_now_add=True)
