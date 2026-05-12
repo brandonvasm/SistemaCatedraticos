0b57f0eeddcbe7651cf1fc67be9639209c78ca94
WillyMaldonado <maldonadoguillermo709@gmail.com>
2026-05-11 19:13:29 -0600

    chore: restrict default API permissions to authenticated users only

--- server/api/settings.py
+++ server/api/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
         'apps.users.infrastructure.authentication.CookieJWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
